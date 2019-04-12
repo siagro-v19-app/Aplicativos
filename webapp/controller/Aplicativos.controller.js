40cf457026a6390bbf6a63dedc1b823a7c6fbea3
matusca96 <gomesloud@gmail.com>
2019-04-12 10:27:10 -0300

    Atualização.

--- webapp/controller/Aplicativos.controller.js
+++ webapp/controller/Aplicativos.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	"sap/ui/model/json/JSONModel"
-], function(Controller, MessageBox, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, MessageBox, JSONModel, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("br.com.idxtecAplicativos.controller.Aplicativos", {
@@ -11,6 +13,17 @@ sap.ui.define([
 			
 			this.getOwnerComponent().setModel(oParamModel, "parametros");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+		
+		filtraApp: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter = new Filter("Codigo", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter
+			];
+
+			this.getView().byId("tableApp").getBinding("rows").filter(aFilters, "Application");
 		},
 
 		onRefresh: function(e){
